90233ad38db484ee286e1ba3ad8bc99550e9f324
Moshood Kareem <mk.bidemi005@gmail.com>
2023-05-09 20:24:03 -0800

    Build

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -5,58 +5,14 @@ import pic1 from "../assets/20230106_184536.jpg"
 const Messages = () => {
     return (
         <div className="messages">
-            <div className="owner">
+            <div className="message owner">
                 <div className="details">
                     <img src={pic1} alt="" />
                     <span>just now</span>
                 </div>
-                <div className="text">
-                    <span>How are you</span>
-                </div>
-            </div>
-            <div className="receiver">
-                <div className="details">
-                    <img src={pic1} alt="" />
-                    <span>just now</span>
-                </div>
-                <div className="text">
-                    <span>How are you</span>
-                </div>
-            </div>
-            <div className="owner">
-                <div className="details">
-                    <img src={pic1} alt="" />
-                    <span>just now</span>
-                </div>
-                <div className="text">
-                    <span>How are you</span>
-                </div>
-            </div>
-            <div className="receiver">
-                <div className="details">
-                    <img src={pic1} alt="" />
-                    <span>just now</span>
-                </div>
-                <div className="text">
-                    <span>How are you</span>
-                </div>
-            </div>
-            <div className="owner">
-                <div className="details">
-                    <img src={pic1} alt="" />
-                    <span>just now</span>
-                </div>
-                <div className="text">
-                    <span>How are you</span>
-                </div>
-            </div>
-            <div className="receiver">
-                <div className="details">
-                    <img src={pic1} alt="" />
-                    <span>just now</span>
-                </div>
-                <div className="text">
-                    <span>How are you</span>
+                <div className="textDetails">
+                     <img src={pic1} alt="" /> 
+                    <p className="text">How are you?</p>
                 </div>
             </div>
         </div>

9fb6ad8ccfb0087321717631c1b572f86b320347
Moshood Kareem <mk.bidemi005@gmail.com>
2023-05-10 10:25:34 -0800

    Chat app

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,79 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import pic1 from "../assets/20230106_184536.jpg"
-
+import React, { useContext, useState } from "react";
+import { collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext"
 
 const Search = () => {
+    const [username, setUserName] = useState("");
+    const [user, setUser] = useState(null);
+    const [err, setErr] = useState(false);
+
+    const { currentUser } = useContext(AuthContext)
+
+    const handleSearch = async () => {
+        const q = query(collection(db, "users"), where("displayName", "==", username)
+        );
+        try {
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                setUser(doc.data())
+            });
+        } catch (err) {
+            setErr(true)
+        }
+    }
+
+    const handleKey = e => {
+        e.code === "Enter" && handleSearch();
+
+    } 
+    const handleSelect = async () => {
+        
+        const combinedId = currentUser.uid > user.uid
+         ? currentUser.uid + user.uid 
+         : user.uid + currentUser.uid;
+         
+         try {
+            const res = await getDoc(doc(db, "chats", combinedId));
+
+            if(!res.exists()) {
+                await setDoc(doc(db, "chats", combinedId ), { messages: [] });
+
+                await updateDoc(doc(db, "userChat", currentUser.uid), {
+                  [combinedId + ".userInfo"]: {
+                    uid: user.uid,
+                    displayName: user.displayName,
+                    photoURL:user.photoURL
+                  },
+                  [combinedId + ".date"]: serverTimestamp()
+                });
+            }
+         } catch (err) {}
+
+         setUser(null);
+         setUserName("")
+    };
     return (
         <div className="search">
             <div className="search-container">
-                <FontAwesomeIcon icon={faSearch} className="icon"/>
-                <input type="search" name="search" id="search" placeholder="Find a user"/>
+                <FontAwesomeIcon icon={faSearch} className="icon" />
+                <input 
+                type="search" 
+                placeholder="Find a user" 
+                onKeyDown={handleKey} 
+                onChange={e => setUserName(e.target.value)} 
+                value={username}
+                />
             </div>
-            <div className="userChat">
-                <img src={pic1} alt="" />
+            {err && <span>User not found!</span> }
+            {user && <div className="userChat" onClick={handleSelect}>
+                <img src={user.photoURL} alt="" />
                 <div className="userChatInfo">
-                    <span className="name">Moshood</span>
+                    <span className="name">{user.displayName}</span>
                 </div>
-            </div>
+            </div>}
         </div>
     )
 }
--- src/components/chats.jsx
+++ src/components/chats.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import pic1 from "../assets/20230106_184536.jpg"
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Chats = () => {
+
+    const [chats, setChats] = useState([]);
+    const { currentUser } = useContext(AuthContext);
+
+
+    useEffect(() => {
+        const getChats = () => {
+            const unsub = onSnapshot(doc(db, "userChat", currentUser.uid), (doc) => {
+                setChats(doc.data());
+            });
+    
+            return() => {
+                unsub();
+            }
+        };
+
+        currentUser.uid && getChats();
+    }, [currentUser.uid]);
+
+    console.log(Object.entries(chats))
     return (
         <div className="chats">
-             <div className="userChat">
-                <img src={pic1} alt="" />
+             {Object.entries(chats)?.map((chat) => (
+                <div className="userChat" key={chat[0]}>
+                <img src={chat[1].userInfo.photoURL} alt="" />
                 <div className="userChatInfo">
-                    <span className="name">Moshood</span>
-                    <span className="lastMessage">Hello</span>
+                    <span className="name">{chat[1].userInfo.displayName}</span>
+                    <span className="lastMessage">{chat[1].userInfo.lastMessage?.text}</span>
                 </div>
             </div>
+             ))}
         </div>
     )
 }
